1b41c8fd5ca6ecf6b03ba6a5800293c686acf4a4
Jacob Hudson <jacob.alan.hudson@gmail.com>
2017-02-27 21:03:13 -0600

    base bcc install

---
+++ scripts/install-bcc-ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "deb [trusted=yes] https://repo.iovisor.org/apt/xenial xenial-nightly main" | sudo tee /etc/apt/sources.list.d/iovisor.list
+sudo apt-get update
+sudo apt-get install bcc-tools
