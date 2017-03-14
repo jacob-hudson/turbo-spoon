3d53b7300524b1689a2ea7137cc087fb347d2f0f
Jacob Hudson <jacob.alan.hudson@gmail.com>
2017-03-13 21:50:21 -0500

    more config options

--- config-sample.rb
+++ config-sample.rb
@@ -1,11 +1,28 @@
 # Config 1 - Single VM, Generic Cluster,  Ubuntu 16.10
+# box = kaorimatz/ubuntu-16.10-amd64
 $boxes = [
   {
     :cluster_size => 1,
-    :box => "kaorimatz/ubuntu-16.10-amd64",
     :ram => "2048",
     :os =>  "ubuntu",
     :os_version => "16.10",
+    :ansible =>
+      [{ :group => "node", :roles => [ "*" ]}],
+    :forwarded_ports =>
+      { 80 => 8480,
+        443 => 8443,
+      },
+  }
+]
+
+# Config 1 - Single VM, Generic Cluster, CentOS 7
+# box = centos/7
+$boxes = [
+  {
+    :cluster_size => 1,
+    :ram => "2048",
+    :os =>  "centos",
+    :os_version => "7",
     :ansible =>
       [{ :group => "node", :roles => [ "*" ]}],
     :forwarded_ports =>
